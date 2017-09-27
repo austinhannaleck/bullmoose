1bb0ecb889ffeb1762fdf602658fa6bcadb9b392
austinhannaleck <ahannaleck1@gmail.com>
2017-09-27 15:22:28 -0400

    Basic checkout functionality complete - need to connect to paypal, add coupon codes, and style

--- CyberCart.js
+++ CyberCart.js
@@ -1,28 +1,62 @@
+// Index for item price in Cart contents object.
+const PRICE_INDEX = 0;
+// Index for item count in Cart contents object.
+const COUNT_INDEX = 1;
+// Index for item description in Cart contents object.
+const ITEM_INDEX  = 2;
+
 $(document).ready(function()
 {
+    
+    var CartButtons = document.getElementsByClassName('cart-button');
+
+    //alert(CartButtons[0].getAttribute("data-productid"));
+    
+    /*
+    for(var i = 0; i < CartButtons.length; i++)
+    {
+        alert(CartButtons[i].getAttribute("data-productid"));
+    }
+    */
+    
+    $('.cart-button').click(function()
+    {
+        var id   = $(this).attr("data-productid");
+        var item = $(this).attr("data-item");
+        
+        Cart["contents"][item][COUNT_INDEX]++;
+        
+        alert(Cart["contents"][item][COUNT_INDEX]);
+        
+    });
+    
     var Cart = 
     {
         subtotal: 0,
-        shipping: 1,
-        /* Change price table accordingly */
-        priceTable:
+        shipping: 0,
+        
+        /* List of items with price and quantity */
+        contents:
         {
-            signatureOil: 12.00,
-            signatureBalm: 12.00,
-            signatureWash: 12.00,
-            expeditionOil: 12.00,
-            expeditionBalm: 12.00,
-            expeditionButter: 12.00,
-            hearthOil: 12.00,
-            hearthBalm: 12.00,
-            hearthWash: 12.00
+            sigBalm: [12.00, 0, "Beard Balm - Signature"],
+            sigOil: [12.00, 0, "Beard Oil - Signature"],
+            sigWash: [12.00, 0, "Beard Wash - Signature"],
+            expedBalm: [12.00, 0, "Beard Balm - Expedition"],
+            expedOil: [12.00, 0, "Beard Oil - Expedition"],
+            expedButter: [12.00, 0, "Shave Butter - Expedition"],
+            hearthBalm: [12.00, 0, "Beard Balm - Hearth"],
+            hearthOil: [12.00, 0, "Beard Oil - Hearth"],
+            hearthWash: [12.00, 0, "Beard Wash - Hearth"]
         }
     };
+    
+    
+    
+    
     
     // Show CyberCart modal when user clicks on cart icon
     $('#cart').click(function()
     {
-        alert(Cart.priceTable['signatureOil']);
         $('#cybercart-modal').css('display', 'block');
         
         UpdateCart();
@@ -39,10 +73,68 @@ $(document).ready(function()
         
     }
     
+        
+    
     function UpdateCart()
-    {
-        $('#shipping').text(Cart.shipping);
-        $('#total').text(Cart.subtotal + Cart.shipping);
+    {   
+        // Clear the cart
+        $('#items').html(""); 
+        Cart.subtotal = 0;
+        
+        // Iterate through number of possible items in cart.
+        // In the event that an item has a quantity greater than 0,
+        // add item to the cart and update price.
+        
+        for(var property in Cart.contents)
+        {
+            if(Cart.contents.hasOwnProperty(property))
+            {
+                if(Cart.contents[property][COUNT_INDEX] > 0)
+                {
+                    
+                    // Add new item to cart. Use data-item to identify the name of object with cart contents, class = item-quantity to keep track of input fields,
+                    $('#items').append("<div data-item=" + property + " class='row'> <div class='col-6'><h3>" + 
+                                        Cart.contents[property][ITEM_INDEX] + "</h3></div>" +
+                                        "<div class='col-4'><input class='item-quantity' min='0' type='number' value=" + Cart.contents[property][COUNT_INDEX] + "></div><div class='col-2'><span class='delete'>" + "&times;" + "</span></div></div>");
+                
+                    Cart.subtotal = Cart.subtotal + (Cart.contents[property][PRICE_INDEX] * Cart.contents[property][COUNT_INDEX]);
+                }
+            }
+        }
+        
+        $('#shipping').text("$" + parseFloat(Cart.shipping).toFixed(2));
+        
+        // Add subtotal to total and force to the 2nd decimal place.
+        $('#total').text("$" + parseFloat( Cart.subtotal + Cart.shipping).toFixed(2));
+        
+        // Add event handlers for quantity buttons in cart
+        $(".item-quantity").change(function()
+        {
+            var count = $(this).val();
+            var item = $(this).parent().parent().attr("data-item"); // Get the data item of the parent of the parent (the row div)
+            
+            // Don't allow users to edit item quantity to less than zero.
+            if(count < 0)
+            {
+                alert("ERROR - Item quantity cannot be less than zero.")
+                count = 0; // Set item to zero to remove from cart.
+            }
+            
+            Cart.contents[item][COUNT_INDEX] = parseFloat(count);
+            UpdateCart();
+        });
+        
+        // Add event handlers for item delete button
+        $('.delete').click(function()
+        {
+            var item = $(this).parent().parent().attr("data-item"); // Get the data item of the parent of the parent (the row div)
+            
+            Cart.contents[item][COUNT_INDEX] = 0;
+            
+            UpdateCart();
+        });
     }
+    
+    
     
 });
